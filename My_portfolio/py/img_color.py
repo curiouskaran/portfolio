447981d44a8ab40c3b03411ddbd0057882b343e0
curiouskaran <01karan95@gmail.com>
2017-01-28 06:22:10 +0530

    script for removing background from images

---
+++ My_portfolio/py/img_color.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+img = Image.open('bootstrap_300.jpg')
+img = img.convert("RGBA")
+datas = img.getdata()
+
+newData = []
+for item in datas:
+    if item[0] == 255 and item[1] == 255 and item[2] == 255:
+        newData.append((255, 255, 255, 0))
+    else:
+        newData.append(item)
+
+img.putdata(newData)
+img.save("boot.png", "PNG")
